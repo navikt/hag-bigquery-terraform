971c850e4dcbdcd80ed620a65abab4bc87acbaa2
Gustav Berggren <gustav.berggren@nav.no>
2023-09-15 16:02:50 +0200

    terraform skjellet

---
+++ dev/main.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_providers {
+        google = {
+            source  = "hashicorp/google"
+            version = "4.55.0"
+        }
+    }
+}
+
+provider "google" {
+    project = var.gcp_project["project"]
+    region  = var.gcp_project["region"]
+}
+
+data "google_project" "project" {}
+
+module "google_storage_bucket" {
+    source = "../modules/google-cloud-storage"
+
+    name     = "hag-bigquery-terraform-state-dev"
+    location = var.gcp_project["region"]
+}
