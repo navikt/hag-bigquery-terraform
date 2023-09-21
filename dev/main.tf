c91ab2ca2437f632b8927025fe639b22941161b0
Gustav Berggren <gustav.berggren@nav.no>
2023-09-21 14:44:45 +0200

    terraform format

--- dev/main.tf
+++ dev/main.tf
@@ -1,25 +1,25 @@
 terraform {
-    required_providers {
-        google = {
-            source  = "hashicorp/google"
-            version = "4.55.0"
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.55.0"
     }
-    backend "gcs" {
-        bucket = "hag-bigquery-terraform-state-dev"
-    }
+  }
+  backend "gcs" {
+    bucket = "hag-bigquery-terraform-state-dev"
+  }
 }
 
 provider "google" {
-    project = var.gcp_project["project"]
-    region  = var.gcp_project["region"]
+  project = var.gcp_project["project"]
+  region  = var.gcp_project["region"]
 }
 
 data "google_project" "project" {}
 
 module "google_storage_bucket" {
-    source = "../modules/google-cloud-storage"
+  source = "../modules/google-cloud-storage"
 
-    name     = "hag-bigquery-terraform-state-dev"
-    location = var.gcp_project["region"]
+  name     = "hag-bigquery-terraform-state-dev"
+  location = var.gcp_project["region"]
 }
--- dev/variables.tf
+++ dev/variables.tf
@@ -1,9 +1,9 @@
 variable "gcp_project" {
-    description = "GCP project and region defaults."
-    type        = map(string)
-    default = {
-        region  = "europe-north1",
-        zone    = "europe-north1-a",
-        project = "helsearbeidsgiver-dev-6d06"
-    }
+  description = "GCP project and region defaults."
+  type        = map(string)
+  default = {
+    region  = "europe-north1",
+    zone    = "europe-north1-a",
+    project = "helsearbeidsgiver-dev-6d06"
+  }
 }
