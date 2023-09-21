ba3214673718392cea9009e68d3ba5dcaf79d622
Gustav Berggren <gustav.berggren@nav.no>
2023-09-21 17:09:57 +0200

    legg til datastream-vpc ip range

--- dev/datastream-vpc.tf
+++ dev/datastream-vpc.tf
@@ -2,3 +2,14 @@ resource "google_compute_network" "hag_datastream_private_vpc" {
   name    = "hag-datastream-vpc"
   project = var.gcp_project["project"]
 }
+
+// The IP-range in the VPC used for the Datastream VPC peering. If a Cloud SQL instance is assigned a private
+// IP address, this is the range it will be assigned from.
+resource "google_compute_global_address" "hag_datastream_vpc_ip_range" {
+  name          = "hag-datastream-vpc-ip-range"
+  project       = var.gcp_project["project"]
+  address_type  = "INTERNAL"
+  purpose       = "VPC_PEERING"
+  network       = google_compute_network.hag_datastream_private_vpc.id
+  prefix_length = 20
+}
