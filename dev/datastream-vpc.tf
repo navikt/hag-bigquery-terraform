0039fa16cfd006861028388ecac703c983acde6d
Gustav Berggren <gustav.berggren@nav.no>
2023-11-02 16:10:59 +0100

    opprett datastream

--- dev/datastream-vpc.tf
+++ dev/datastream-vpc.tf
@@ -39,7 +39,7 @@ resource "google_compute_firewall" "allow_datastream_to_cloud_sql" {
   allow {
     protocol = "tcp"
     ports = [
-      var.bro_db_cloud_sql_port,
+      var.bro_db_cloud_sql_port
     ]
   }
 
@@ -114,4 +114,53 @@ resource "google_datastream_connection_profile" "datastream_bigquery_connection_
   connection_profile_id = "datastream-bigquery-connection-profile"
 
   bigquery_profile {}
+}
+
+resource "google_datastream_stream" "bro_datastream" {
+  stream_id     = "bro-datastream"
+  display_name  = "bro-datastream"
+  desired_state = "RUNNING"
+  project       = var.gcp_project["project"]
+  location      = var.gcp_project["region"]
+  labels        = {}
+  backfill_all {}
+  timeouts {}
+
+  source_config {
+    source_connection_profile = google_datastream_connection_profile.bro_postgresql_connection_profile.id
+
+    postgresql_source_config {
+      max_concurrent_backfill_tasks = 0
+      publication                   = "bro_publication"
+      replication_slot              = "bro_replication"
+
+      exclude_objects {
+        postgresql_schemas {
+          schema = "public"
+
+          postgresql_tables {
+            table = "flyway_schema_history"
+          }
+        }
+      }
+
+      include_objects {
+        postgresql_schemas {
+          schema = "public"
+        }
+      }
+    }
+  }
+
+  destination_config {
+    destination_connection_profile = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
+
+    bigquery_destination_config {
+      data_freshness = "300s"
+
+      single_target_dataset {
+        dataset_id = "${var.gcp_project["project"]}:${google_bigquery_dataset.bro_dataset.dataset_id}"
+      }
+    }
+  }
 }
