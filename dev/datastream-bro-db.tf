2fea522a868b0769f6ddaba2822c47163a998a6e
Gustav Berggren <gustav.berggren@nav.no>
2023-11-16 13:52:43 +0100

    legg til datastream igjen

--- dev/datastream-bro-db.tf
+++ dev/datastream-bro-db.tf
@@ -34,3 +34,52 @@ resource "google_datastream_connection_profile" "bro_postgresql_connection_profi
   }
 }
 
+resource "google_datastream_stream" "bro_datastream" {
+  stream_id     = "bro-datastream"
+  display_name  = "bro-datastream"
+  desired_state = "RUNNING"
+  project       = var.gcp_project["project"]
+  location      = var.gcp_project["region"]
+  labels        = {}
+  backfill_all {}
+  timeouts {}
+
+  source_config {
+    source_connection_profile = google_datastream_connection_profile.bro_postgresql_connection_profile.id
+
+    postgresql_source_config {
+      max_concurrent_backfill_tasks = 0
+      publication                   = "bro_publication"
+      replication_slot              = "bro_replication"
+
+      exclude_objects {
+        postgresql_schemas {
+          schema = "public"
+
+          postgresql_tables {
+            table = "flyway_schema_history"
+          }
+        }
+      }
+
+      include_objects {
+        postgresql_schemas {
+          schema = "public"
+        }
+      }
+    }
+  }
+
+  destination_config {
+    destination_connection_profile = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
+
+    bigquery_destination_config {
+      data_freshness = "900s"
+
+      single_target_dataset {
+        dataset_id = "${var.gcp_project["project"]}:${google_bigquery_dataset.bro_dataset.dataset_id}"
+      }
+    }
+  }
+}
+
