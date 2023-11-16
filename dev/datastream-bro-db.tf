931b6c16f290458371f88ea8f30f3ef7b0c13ae7
Gustav Berggren <gustav.berggren@nav.no>
2023-11-16 14:29:36 +0100

    endre data freshness

--- dev/datastream-bro-db.tf
+++ dev/datastream-bro-db.tf
@@ -74,7 +74,7 @@ resource "google_datastream_stream" "bro_datastream" {
     destination_connection_profile = google_datastream_connection_profile.datastream_bigquery_connection_profile.id
 
     bigquery_destination_config {
-      data_freshness = "900s"
+      data_freshness = "300s"
 
       single_target_dataset {
         dataset_id = "${var.gcp_project["project"]}:${google_bigquery_dataset.bro_dataset.dataset_id}"
