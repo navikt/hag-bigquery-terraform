5b267de5171bb7f5532c7172aabf20f01437eda4
Gustav Berggren <gustav.berggren@nav.no>
2025-04-25 13:39:30 +0200

    lag view for simba inntektsmelding tabell i prod

---
+++ prod/views-simba.tf
@@ -0,0 +1,93 @@
+module "simba_inntektsmelding_view" {
+  source              = "../modules/google-bigquery-view"
+  deletion_protection = false
+  dataset_id          = "simba_dataset"
+  view_id             = "public_inntektsmelding_view"
+  view_schema = jsonencode(
+    [
+      {
+        name = "id"
+        type = "INTEGER"
+      },
+      {
+        name = "dokument"
+        type = "JSON"
+      },
+      {
+        name = "forespoersel_id"
+        type = "STRING"
+      },
+      {
+        name = "journalpost_id"
+        type = "STRING"
+      },
+      {
+        name = "innsendt"
+        type = "TIMESTAMP"
+      },
+      {
+        name = "ekstern_inntektsmelding"
+        type = "JSON"
+      },
+      {
+        name = "skjema"
+        type = "JSON"
+      },
+      {
+        name = "inntektsmelding_id"
+        type = "STRING"
+      },
+      {
+        name = "avsender_navn"
+        type = "STRING"
+      },
+      {
+        name = "datastream_metadata"
+        type = "RECORD"
+        fields = [
+          {
+            name = "uuid"
+            type = "STRING"
+          },
+          {
+            name = "source_timestamp"
+            type = "INTEGER"
+          },
+          {
+            name = "change_sequence_number"
+            type = "STRING"
+          },
+          {
+            name = "change_type"
+            type = "STRING"
+          },
+          {
+            name = "sort_keys"
+            type = "STRING"
+            mode = "REPEATED"
+          }
+        ]
+      },
+      {
+        name = "row_num"
+        type = "INTEGER"
+      }
+    ]
+  )
+  view_query = <<EOF
+SELECT *
+FROM (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (
+      PARTITION BY id
+      ORDER BY datastream_metadata.source_timestamp DESC
+    ) AS row_num
+  FROM
+    `${var.gcp_project["project"]}.simba_dataset.public_inntektsmelding`
+  WHERE
+    datastream_metadata.change_type != 'DELETE'
+)
+WHERE row_num = 1
+EOF
+}
