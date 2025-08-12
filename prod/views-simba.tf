2a1c2a7d4ed5efa062372af0e3e50fd9ed073861
Gustav Berggren <gustav.berggren@nav.no>
2025-08-12 08:51:40 +0200

    Legger til view for selvbestemt inntektsmelding

--- prod/views-simba.tf
+++ prod/views-simba.tf
@@ -91,3 +91,90 @@ FROM (
 WHERE row_num = 1
 EOF
 }
+
+module "simba_selvbestemt_inntektsmelding_view" {
+  source              = "../modules/google-bigquery-view"
+  deletion_protection = false
+  dataset_id          = "simba_dataset"
+  view_id             = "public_inntektsmelding_view"
+  view_schema = jsonencode(
+    [
+      {
+        name = "id"
+        type = "INTEGER"
+      },
+      {
+        name = "inntektsmelding_id"
+        type = "STRING"
+      },
+      {
+        name = "selvbestemt_id"
+        type = "STRING"
+      },
+      {
+        name = "inntektsmelding"
+        type = "JSON"
+      },
+      {
+        name = "journalpost_id"
+        type = "STRING"
+      },
+      {
+        name = "opprettet"
+        type = "TIMESTAMP"
+      },
+      {
+        name = "datastream_metadata"
+        type = "RECORD"
+        fields = [
+          {
+            name = "uuid"
+            type = "STRING"
+          },
+          {
+            name = "source_timestamp"
+            type = "INTEGER"
+          },
+          {
+            name = "change_sequence_number"
+            type = "STRING"
+          },
+          {
+            name = "change_type"
+            type = "STRING"
+          },
+          {
+            name = "sort_keys"
+            type = "STRING"
+            mode = "REPEATED"
+          }
+        ]
+      },
+      {
+        name = "prosessert"
+        type = "TIMESTAMP"
+      },
+      {
+        name = "row_num"
+        type = "INTEGER"
+      }
+    ]
+  )
+  view_query = <<EOF
+SELECT *
+FROM (
+  SELECT
+    *,
+    ROW_NUMBER() OVER (
+      PARTITION BY id
+      ORDER BY datastream_metadata.source_timestamp DESC
+    ) AS row_num
+  FROM
+    `${var.gcp_project["project"]}.simba_dataset.public_selvbestemt_inntektsmelding`
+  WHERE
+    datastream_metadata.change_type != 'DELETE'
+)
+WHERE row_num = 1
+EOF
+}
+
