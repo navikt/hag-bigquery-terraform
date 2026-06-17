eef9eb2c0820092d3d3797e2161a6fae5f6729ae
Gustav Berggren <gustav.berggren@nav.no>
2026-06-17 10:25:28 +0200

    Legg til spinn dataset som authorized dataset

--- prod/datastreams.tf
+++ prod/datastreams.tf
@@ -160,6 +160,12 @@ module "spinosaurus_datastream" {
     },
     {
       dataset = {
+        dataset_id = "spinn_dataset"
+        project_id = var.gcp_project["project"]
+      }
+    },
+    {
+      dataset = {
         dataset_id = "lps_api_grafana_views"
         project_id = var.gcp_project["project"]
       }
@@ -168,7 +174,7 @@ module "spinosaurus_datastream" {
       dataset = {
         dataset_id = "helseytelser_data_deling"
         project_id = var.gcp_project["project"]
-      }
+      },
   }]
 }
 
